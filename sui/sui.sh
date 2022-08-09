4dec45e9697bb8eec7b91b8b99d67ac9bac38d78
NodeX | WannaCry? <nodexploit@gmail.com>
2022-08-09 20:16:28 +0000

    Fix Output

--- sui/sui.sh
+++ sui/sui.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "=========================================="
 echo -e "\033[0;35m"
 echo "       ::::    :::  ::::::::  :::::::::  :::::::: ::::     ::::  ";
@@ -10,6 +11,7 @@ echo "  #+#   #+#+# #+#    #+# #+#    #+# #+#       #+#     #+#        ";
 echo " ###    ####  ########  #########  ######## ###        ###       ";
 echo -e "\e[0m"
 echo "=========================================="
+
 sleep 2
 
 echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
@@ -18,65 +20,40 @@ sudo apt update && sudo apt upgrade -y
 
 echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
 # packages
-sudo apt install tzdata git ca-certificates curl build-essential libssl-dev pkg-config libclang-dev cmake jq -y --no-install-recommends
-sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && sudo chmod +x /usr/local/bin/yq
-# install rust
-sudo curl https://sh.rustup.rs -sSf | sh -s -- -y
-source $HOME/.cargo/env
+sudo wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.23.1/yq_linux_amd64 && chmod +x /usr/local/bin/yq
+sudo apt-get install jq -y
 
-echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
-# download binary
-sudo mkdir -p /var/sui
-cd $HOME && rm sui -rf
-git clone https://github.com/MystenLabs/sui.git && cd sui
-git remote add upstream https://github.com/MystenLabs/sui
-git fetch upstream
-git checkout --track upstream/devnet
-cargo build --release -p sui-node
-sudo mv ~/sui/target/release/sui-node /usr/local/bin/
+# download sui binaries
+version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
+wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
 
-# set configuration
-wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-sudo cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
-sudo yq e -i '.db-path="/var/sui/db"' /var/sui/fullnode.yaml \
-&& yq e -i '.genesis.genesis-file-location="/var/sui/genesis.blob"' /var/sui/fullnode.yaml \
-&& yq e -i '.metrics-address="0.0.0.0:9184"' /var/sui/fullnode.yaml \
-&& yq e -i '.json-rpc-address="0.0.0.0:9000"' /var/sui/fullnode.yaml
+# download and update configs
+mkdir -p $HOME/.sui
+wget -qO $HOME/.sui/fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+yq -i ".db-path = \"$HOME/.sui/db\"" $HOME/.sui/fullnode.yaml
+yq -i '.metrics-address = "0.0.0.0:9184"' $HOME/.sui/fullnode.yaml
+yq -i '.json-rpc-address = "0.0.0.0:9000"' $HOME/.sui/fullnode.yaml
+yq -i ".genesis.genesis-file-location = \"$HOME/.sui/genesis.blob\"" $HOME/.sui/fullnode.yaml
 
-# start service
-echo "[Unit]
-Description=Sui Node
-After=network.target
+# create sui service
+sudo tee /etc/systemd/system/suid.service > /dev/null <<EOF
+[Unit]
+Description=Sui node
+After=network-online.target
 
 [Service]
 User=$USER
-Type=simple
-ExecStart=/usr/local/bin/sui-node --config-path /var/sui/fullnode.yaml
+ExecStart=$(which sui-node) --config-path $HOME/.sui/fullnode.yaml
 Restart=on-failure
+RestartSec=3
 LimitNOFILE=65535
 
 [Install]
-WantedBy=multi-user.target" > $HOME/suid.service
-mv $HOME/suid.service /etc/systemd/system/
-
-sudo tee <<EOF >/dev/null /etc/systemd/journald.conf
-Storage=persistent
+WantedBy=multi-user.target
 EOF
 
-sudo systemctl restart systemd-journald
+# start sui node
 sudo systemctl daemon-reload
 sudo systemctl enable suid
 sudo systemctl restart suid
-
-echo "==================================================="
-echo -e '\e[32mCheck node status\e[39m' && sleep 1
-if [[ `service suid status | grep active` =~ "running" ]]; then
-  echo -e "Your Sui node \e[32minstalled and running\e[39m!"
-else
-  echo -e "Your Sui node \e[31mwas not installed correctly\e[39m, please reinstall."
-fi
-echo -e "Check your node logs: \e[journalctl -fu suid -o cat\e[39m\n\n"
-
-echo -e "To register your Sui node:"
-echo -e "1. Join Sui Discord: \e[32mhttps://discord.gg/b5vWu33f\e[39m"
-echo -e "2. Paste your node rpc endpoint url \e[32mhttp://$(curl -s ifconfig.me):9000\e[39m into \e[32m#📋node-ip-application\e[39m channel"
