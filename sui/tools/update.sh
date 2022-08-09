def8322299d33940722085c5e634fb51c5346c91
NodeX | WannaCry? <nodexploit@gmail.com>
2022-08-09 20:18:46 +0000

    Update update.sh

--- sui/tools/update.sh
+++ sui/tools/update.sh
@@ -1,16 +1,12 @@
 echo -e "\e[1m\e[32mUpdating Sui fullnode... \e[0m" && sleep 1
-sudo systemctl stop suid
-sudo rm -rf /var/sui/db/* /var/sui/genesis.blob
-source $HOME/.cargo/env
-cd $HOME/sui
-git fetch upstream
-git checkout -B devnet --track upstream/devnet
-cargo build --release -p sui-node
-mv ~/sui/target/release/sui-node /usr/local/bin/
-wget -O /var/sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
-sudo cp crates/sui-config/data/fullnode-template.yaml /var/sui/fullnode.yaml
-sudo yq e -i '.db-path="/var/sui/db"' /var/sui/fullnode.yaml \
-&& yq e -i '.genesis.genesis-file-location="/var/sui/genesis.blob"' /var/sui/fullnode.yaml \
-&& yq e -i '.metrics-address="0.0.0.0:9184"' /var/sui/fullnode.yaml \
-&& yq e -i '.json-rpc-address="0.0.0.0:9000"' /var/sui/fullnode.yaml
-sudo systemctl start suid
+systemctl stop suid
+wget -qO $HOME/.sui/fullnode.yaml https://github.com/MystenLabs/sui/raw/main/crates/sui-config/data/fullnode-template.yaml
+wget -qO $HOME/.sui/genesis.blob https://github.com/MystenLabs/sui-genesis/raw/main/devnet/genesis.blob
+yq -i ".db-path = \"$HOME/.sui/db\"" $HOME/.sui/fullnode.yaml
+yq -i '.metrics-address = "0.0.0.0:9184"' $HOME/.sui/fullnode.yaml
+yq -i '.json-rpc-address = "0.0.0.0:9000"' $HOME/.sui/fullnode.yaml
+yq -i ".genesis.genesis-file-location = \"$HOME/.sui/genesis.blob\"" $HOME/.sui/fullnode.yaml
+rm -rf $HOME/.sui/db
+version=$(wget -qO- https://api.github.com/repos/SecorD0/Sui/releases/latest | jq -r ".tag_name")
+wget -qO- "https://github.com/SecorD0/Sui/releases/download/${version}/sui-linux-amd64-${version}.tar.gz" | sudo tar -C /usr/local/bin/ -xzf -
+systemctl restart suid
