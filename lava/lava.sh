49b6718e15dbf904e4affd04949fcd9efd8457ae
Whyme | NodeX Capital <nodexploit@gmail.com>
2023-01-05 21:15:00 +0700

    Add files via upload

---
+++ lava/lava.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+echo -e "\033[0;31m"
+echo " ::::    :::  ::::::::  :::::::::  :::::::: ::::     :::: ";
+echo " :+:+:   :+: :+:    :+: :+:    :+: :+:       :+:     :+:  ";
+echo " :+:+:+  +:+ +:+    +:+ :+:    :+: +:+        +:+   +:+   ";
+echo " +#+ +:+ +#+ +#+    +:+ +#+    +:+ +#++:++#      #+#      ";
+echo " +#+  +#+#+# +#+    +#+ +#+    +:+ +#+        +#+   +#+   ";
+echo " #+#   #+#+# #+#    #+# #+#    #+# #+#       #+#     #+#  ";
+echo " ###    ####  ########  #########  ######## ###       ### ";
+echo -e "\e[0m"
+
+sleep 2
+
+# set vars
+if [ ! $NODENAME ]; then
+	read -p "Enter node name: " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+LAVA_PORT=36
+if [ ! $WALLET ]; then
+	echo "export WALLET=wallet" >> $HOME/.bash_profile
+fi
+echo "export LAVA_CHAIN_ID=lava-testnet-1" >> $HOME/.bash_profile
+echo "export LAVA_PORT=${LAVA_PORT}" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+echo '================================================='
+echo -e "Moniker Name: \e[1m\e[32m$NODENAME\e[0m"
+echo -e "Wallet Name: \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain Name: \e[1m\e[32m$LAVA_CHAIN_ID\e[0m"
+echo -e "Lava Custom Port: \e[1m\e[32m$LAVA_PORT\e[0m"
+echo '================================================='
+sleep 2
+
+echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
+# update
+sudo apt update && sudo apt upgrade -y
+
+echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
+# packages
+sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
+
+# install go
+if ! [ -x "$(command -v go)" ]; then
+  ver="1.19.3"
+  cd $HOME
+  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
+  sudo rm -rf /usr/local/go
+  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
+  rm "go$ver.linux-amd64.tar.gz"
+  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+  source ~/.bash_profile
+fi
+
+echo -e "\e[1m\e[32m3. Downloading & building binaries... \e[0m" && sleep 1
+# download binary
+cd $HOME
+git clone https://github.com/K433QLtr6RA9ExEq/GHFkqmTzpdNLDd6T.git
+wget https://lava-binary-upgrades.s3.amazonaws.com/testnet/v0.3.0/lavad
+chmod +x lavad
+mv lavad $HOME/go/bin/
+
+echo -e "\e[1m\e[32m3. Setting up config... \e[0m" && sleep 1
+# config
+lavad config chain-id $LAVA_CHAIN_ID
+lavad config keyring-backend test
+lavad config node tcp://localhost:${LAVA_PORT}657
+
+echo -e "\e[1m\e[32m3. Init Lavad Network... \e[0m" && sleep 1
+# init
+lavad init $NODENAME --chain-id $LAVA_CHAIN_ID
+
+echo -e "\e[1m\e[32m3. Downloading and genesis & addrbook... \e[0m" && sleep 1
+# download genesis and addrbook
+cp $HOME/GHFkqmTzpdNLDd6T/testnet-1/genesis_json/genesis.json $HOME/.lava/config
+wget -O $HOME/.lava/config/addrbook.json "https://raw.githubusercontent.com/obajay/nodes-Guides/main/Lava_Network/addrbook.json"
+
+echo -e "\e[1m\e[32m3. Setting up peers,seed & gas prices... \e[0m" && sleep 1
+# set peers and seeds and gas
+sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0ulava\"/" $HOME/.lava/config/app.toml
+sed -i -e "s/^filter_peers *=.*/filter_peers = \"true\"/" $HOME/.lava/config/config.toml
+external_address=$(wget -qO- eth0.me) 
+sed -i.bak -e "s/^external_address *=.*/external_address = \"$external_address:26656\"/" $HOME/.lava/config/config.toml
+peers="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@prod-pnet-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@prod-pnet-seed-node2.lavanet.xyz:26656"
+sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$peers\"/" $HOME/.lava/config/config.toml
+seeds=""
+sed -i.bak -e "s/^seeds =.*/seeds = \"$seeds\"/" $HOME/.lava/config/config.toml
+sed -i 's/max_num_inbound_peers =.*/max_num_inbound_peers = 50/g' $HOME/.lava/config/config.toml
+sed -i 's/max_num_outbound_peers =.*/max_num_outbound_peers = 50/g' $HOME/.lava/config/config.toml
+
+echo -e "\e[1m\e[32m3. Setting up lava custom port... \e[0m" && sleep 1
+# set custom ports
+sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${LAVA_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${LAVA_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${LAVA_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${LAVA_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${LAVA_PORT}660\"%" $HOME/.lava/config/config.toml
+sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${LAVA_PORT}317\"%; s%^address = \":8080\"%address = \":${LAVA_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${LAVA_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${LAVA_PORT}091\"%" $HOME/.lava/config/app.toml
+
+echo -e "\e[1m\e[32m3.Setting up pruning... \e[0m" && sleep 1
+# config pruning
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="10"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.lava/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.lava/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.lava/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.lava/config/app.toml
+sed -i -e "s/^pruning *=.*/pruning = \"nothing\"/" $HOME/.lava/config/app.toml
+indexer="null" && \
+sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.lava/config/config.toml
+
+echo -e "\e[1m\e[32m3. Enable Prometheus... \e[0m" && sleep 1
+# enable prometheus
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.lava/config/config.toml
+
+echo -e "\e[1m\e[32m3. Reset data blockchain... \e[0m" && sleep 1
+# reset
+lavad tendermint unsafe-reset-all --home $HOME/.lava
+
+echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/lavad.service > /dev/null <<EOF
+[Unit]
+Description=lavad
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which lavad) start --home $HOME/.lava
+Restart=on-failure
+RestartSec=3
+LimitNOFILE=65535
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# start service
+sudo systemctl daemon-reload
+sudo systemctl enable lavad
+sudo systemctl restart lavad && sudo journalctl -u lavad -f -o cat
+
+echo '=============== SETUP FINISHED ==================='
+echo -e 'To check logs: \e[1m\e[32mjournalctl -u lavad -f -o cat\e[0m'
+echo -e "To check sync status: \e[1m\e[32mcurl -s localhost:${LAVA_PORT}657/status | jq .result.sync_info\e[0m"
