6a5df445bed9fed98f5e262ff55ba9897a2b8e56
Whyme | NodeX Capital <nodexploit@gmail.com>
2023-03-12 09:33:20 +0000

    Update go version 16

--- nolus/nolus.sh
+++ nolus/nolus.sh
@@ -78,7 +78,7 @@ sudo apt -qy upgrade
 
 # Install GO
 sudo rm -rf /usr/local/go
-curl -Ls https://go.dev/dl/go1.19.5.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+curl -Ls https://go.dev/dl/go1.19.6.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
