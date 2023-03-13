e3c85793756264abd3342604e9197e72a89d2b2b
Whyme | NodeX Capital <nodexploit@gmail.com>
2023-03-13 07:25:20 +0000

    Update: fix libwasmvm.x86_64.so errors
    - add environment in service

--- okp4/okp4.sh
+++ okp4/okp4.sh
@@ -136,6 +136,7 @@ LimitNOFILE=65535
 Environment="DAEMON_HOME=$HOME/$FOLDER"
 Environment="DAEMON_NAME=$BINARY"
 Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:$HOME/.okp4d/cosmovisor/current/bin"
 
 [Install]
 WantedBy=multi-user.target
